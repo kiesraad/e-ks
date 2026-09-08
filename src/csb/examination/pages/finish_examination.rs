850b0926538e1b1c5d8ea6520e4ee3e974f8c84e
Stefan van Ieperen <stefan@tweedegolf.com>
2026-09-08 12:15:42 +0200

    Show political parties in a table

--- src/csb/examination/pages/finish_examination.rs
+++ src/csb/examination/pages/finish_examination.rs
@@ -2,23 +2,33 @@ use askama::Template;
 use axum::response::{IntoResponse, Response};
 
 use crate::{
-    AppError, Context, CsbContext, HtmlTemplate, csb::examination::paths::CsbFinishExaminationPath,
+    AppError, Context, CsbContext, HtmlTemplate,
+    csb::examination::{
+        extractors::{CsbPoliticalGroup, CsbPoliticalGroups},
+        paths::CsbFinishExaminationPath,
+    },
     filters,
 };
 
 #[derive(Template)]
 #[template(path = "csb/examination/pages/finish_examination.html")]
 struct CsbFinishExaminationTemplate {
-    name: String,
+    political_groups: Vec<CsbPoliticalGroup>,
 }
 
 pub async fn finish(
     _: CsbFinishExaminationPath,
     context: CsbContext,
+    CsbPoliticalGroups(political_groups): CsbPoliticalGroups,
 ) -> Result<Response, AppError> {
+    let finished_political_groups = political_groups
+        .into_iter()
+        .filter(|pg| pg.is_examination_finished)
+        .collect();
+
     Ok(HtmlTemplate(
         CsbFinishExaminationTemplate {
-            name: "World".to_string(),
+            political_groups: finished_political_groups,
         },
         context,
     )
